659ab2beffeaf03f3d90e75585d2c3ed1a49c95d
Maksim Shumko <mszumko@gmail.com>
2022-06-09 22:35:03 +0200

    refactor

--- lib/menu/menu_injector.dart
+++ lib/menu/menu_injector.dart
@@ -1,4 +1,3 @@
-import 'package:snake/database/drift/drift_database.dart';
 import 'package:snake/injecton_container.dart';
 import 'package:snake/menu/bloc/menu_bloc.dart';
 
@@ -6,12 +5,6 @@ mixin MenuInjector on Injector {
   @override
   Future<void> init() async {
     await super.init();
-
-    // repositories
-    final MyDriftDatabase db = getIt();
-    final scoreDao = db;
-
-    // blocs
     getIt.registerLazySingleton(() => MenuBloc());
   }
 }
